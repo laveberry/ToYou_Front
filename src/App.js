04e393eecc19bb4bf0dee3342bd62fdbd7d91405
kadun1 <kadunjj@gmail.com>
2021-07-21 20:46:47 +0900

    ToYou Front
    	첫번째 커밋

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  // message 초기값을 ""으로 설정.
+  const [message, setMessage] = useState("");
+  // useEffect(함수,배열) : 컴포넌트가 화면에 나타났을(마운트)때 자동 실행.
+  useEffect( () => {
+
+    // fetch(url,options) : HTTP 요청 함수
+    fetch('/hello')
+        .then(response => response.text())
+        .then(message => {
+          setMessage(message);
+        });
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">{message}</h1>
+          <p>
+            메시지 출력
+          </p>
+        </header>
+      </div>
   );
 }
 

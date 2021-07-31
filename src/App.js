b46fc0b278ba854c8326bc4f0010a674e76e41f8
leeheeweon <ggoddii@naver.com>
2021-07-31 16:28:58 +0900

    깃 다시 pull

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+import logo from './logo.svg';
 import React, {useState, useEffect} from 'react';
-import axios from 'axios'
 import './App.css';
-import Main from './components/main/Main';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 function App() {
-  //[변수, 변수재정의] = useState("초기값")
-  let [message, setMessage] = useState("");
-  const sburl = "http://localhost:8080";
-  axios.get(sburl+"/hello").then(res=>{
-    console.log(res.data);
-    setMessage(res.data);
-  },[]);
+  // message 초기값을 ""으로 설정.
+  const [message, setMessage] = useState("");
+  // useEffect(함수,배열) : 컴포넌트가 화면에 나타났을(마운트)때 자동 실행.
+  useEffect( () => {
+
+    // fetch(url,options) : HTTP 요청 함수
+    fetch('/hello')
+        .then(response => response.text())
+        .then(message => {
+          setMessage(message);
+        });
+  },[])
 
   return (
       <div className="App">
-        <Main></Main>
+        <header className="App-header">
+          <h1 className="App-title">{message}</h1>
+          <p>
+            메시지 출력
+          </p>
+        </header>
       </div>
   );
 }
